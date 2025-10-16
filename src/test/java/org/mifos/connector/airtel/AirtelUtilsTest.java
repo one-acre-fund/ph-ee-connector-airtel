cde9d03f82f45ae039604f1582e508b2894febfe
Vikeln <vikelnthuku@gmail.com>
2025-10-16 10:36:08 +0300

    chore: update linting errors

--- src/test/java/org/mifos/connector/airtel/AirtelUtilsTest.java
+++ src/test/java/org/mifos/connector/airtel/AirtelUtilsTest.java
@@ -1,23 +1,24 @@
 package org.mifos.connector.airtel;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mifos.connector.airtel.dto.ChannelValidationResponse;
 import org.mifos.connector.airtel.util.AirtelUtils;
 import org.mifos.connector.common.gsma.dto.CustomData;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mifos.connector.airtel.zeebe.ZeebeVariables.PAYMENT_SCHEME;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mifos.connector.airtel.zeebe.ZeebeVariables.AIRTEL_CONSTANT;
+import static org.mifos.connector.airtel.zeebe.ZeebeVariables.PAYMENT_SCHEME;
 
-class AirtelUtilsTest {
+public class AirtelUtilsTest {
 
     @Test
     void testCreateCustomData_containsPaymentScheme() {
-        ChannelValidationResponse response = new ChannelValidationResponse(true, "AMS", "tenant-1", "txn-1", "100", "USD", "12345", "John Doe", List.of(), "Validation successful");
+        ChannelValidationResponse response = new ChannelValidationResponse(true, "AMS",
+                "tenant-1", "txn-1", "100", "USD", "12345",
+                "John Doe", List.of(), "Validation successful");
         List<CustomData> customDataList = AirtelUtils.createCustomData(response, "shortCode", "PT1H");
-
         boolean found = customDataList.stream()
             .anyMatch(cd -> PAYMENT_SCHEME.equals(cd.getKey()) && AIRTEL_CONSTANT.equals(cd.getValue()));
 
